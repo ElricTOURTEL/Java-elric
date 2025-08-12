c1c990c6aa33994fae472040524db88311bfef71
Elric TOURTEL <tourtel.elric@gmail.com>
2025-08-12 14:46:54 +0200

    Utilisation d'un seul print

--- On demenage/src/Main.java
+++ On demenage/src/Main.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.print("Veuillez saisir le nombre de cartons: ");
+        System.out.print("Veuillez saisir le nombre de cartons puis la capacité par voyage (Séparé les input par un espace): ");
         int totalBoxes = clavier.nextInt();
-        System.out.print("Quelle capacité a votre camion ? ");
         int boxesThisTrip = clavier.nextInt();
         Moving moving = new Moving(totalBoxes, boxesThisTrip);
         moving.PerformMoving();
